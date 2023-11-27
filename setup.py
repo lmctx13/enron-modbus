bd19dfb8c8fa6600f8f92fd25332015addb507c3
Leah C <58370350+lmctx13@users.noreply.github.com>
2023-11-27 16:27:21 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='enron-modbus',
+   version='1.0',
+   description='Modbus that includes 32 bit registers',
+   author='u9n',
+   author_email='foomail@foo.example',
+   packages=['enron-modbus'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
